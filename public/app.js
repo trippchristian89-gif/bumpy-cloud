e57377f159873441594fdad55dc15b17f9e3cff7
trippchristian89-gif <tripp.christian89@gmail.com>
2026-03-03 23:34:01 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -240,7 +240,7 @@ window.addEventListener("DOMContentLoaded", () => {
        
       btn.title = "Zu meinem Standort";
       btn.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27"
+        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10"
              viewBox="0 0 24 24" fill="none" stroke="currentColor"
              stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
           <circle cx="12" cy="12" r="3"/>
@@ -279,5 +279,6 @@ function updateMapMarker() {
     gpsMarker.setStyle({ color: "#f97316", fillColor: "#f97316" });
   }
 }
+
 
 
