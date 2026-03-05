69d987e5a6b56821433bb8100b9a9362bcb18eab
trippchristian89-gif <tripp.christian89@gmail.com>
2026-03-05 20:25:55 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -258,7 +258,7 @@ window.addEventListener("DOMContentLoaded", () => {
       const container = L.DomUtil.create("div", "leaflet-bar leaflet-control");
       const btn = L.DomUtil.create("a", "", container);
       btn.title = "Vollbild";
-      btn.href  = "#";
+      btn.href = "javascript:void(0)";
       btn.style.cssText = `
         width: 26px; height: 26px; line-height: 26px;
         display: block; text-align: center;
@@ -333,6 +333,7 @@ function updateMapMarker() {
     if (dx > 0 && currentPage === 2) goToPage(1);
   }, { passive: true });
 })();
+
 
 
 
